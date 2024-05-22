c0ed6d927f953de5815f9c83fb1b7feaa8bd7ae4
bhartigarg06 <166522446+bhartigarg06@users.noreply.github.com>
2024-05-22 11:45:47 +0530

    server and folder

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 console.log ("server and folder");
-const express = require('express')
+const express = require('express');
+// import database connection
+const {connectionDB} = require ("./config/db");
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+  // call  mongodb function
+  connectionDB();
+});
+// 
