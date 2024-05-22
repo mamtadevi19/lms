dbbe27eb2b0a11f7b0116e410d37bb8e35df29c3
bhartigarg06 <166522446+bhartigarg06@users.noreply.github.com>
2024-05-22 12:05:05 +0530

    server and  folder

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,19 @@
-const mongoose = require ('mongoose');
+const mongoose = require ("mongoose");
 
-exports. connectDB = () => {
-    mongoose . connect(
+exports.connectDB = () => {
+    mongoose
+    .connect(
         "mongodb+srv://gargbharti20000:8295536029@cluster0.dnb7qh6.mongodb.net/Olearning"
     )
-    .then(() => console .log ("database connection successfully"))
+    .then(() => console.log ("database connected successfully!"))
     .catch((error) => {
-        console . log  ("database connection failed !");
-        console .lob (error);
-        process. exit(1)
+        console.log  ("database connection failed!");
+        console.log (error);
+        process. exit(1);
 
- } );
+ });
 
+};
 
 
 
--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
 console.log ("server and folder");
 const express = require('express');
 // import database connection
-const {connectionDB} = require ("./config/db");
+const {connectDB} = require ("./config/db");
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-// });
+ });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-  // call  mongodb function
-  connectionDB();
+  // call the mongodb function
+  connectDB();
 });
-// 
