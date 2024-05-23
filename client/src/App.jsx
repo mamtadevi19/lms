fb15f3c8696f687400b484831041bf5629c20a78
mamtadevi19 <166403252+mamtadevi19@users.noreply.github.com>
2024-05-23 14:51:45 +0530

    error fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,33 +4,33 @@ import About from "./pages/About";
 import Catalog from "./pages/Catalog";
 import ContactUs from "./pages/ContactUs";
 import DashBoard from "./pages/DashBoard";
-import  Error  from "./pages/Error";
-import  ForgotPassword  from "./pages/ForgotPassword";
+import Error from "./pages/Error";
+import ForgotPassword from "./pages/ForgotPassword";
 import Login from "./pages/Login";
 import ResetPassword from "./pages/ResetPassword";
-import SearchCourse from " ./pages/SearchCourse";
+import SearchCourse from "./pages/SearchCourse";
 import SignUp from "./pages/SignUp";
 import VerifyOtp from "./pages/VerifyOtp";
-import ViewCourse from "./pages/ViewCourse";
-import SignUp from "./pages/SignUp";
+import ViewCourse from "./pages/ViewCourse"
+
 const App = () => {
   return (
     <div>
       <Routes>
 
-        <Routes path="/" element={<Home />} />
-        <Routes path="/about" element={<About />} />
-        <Routes path="/catalog" element={<Catalog/>} />
-        <Routes path="/contactus" element={<ContactUs/>} />
-        <Routes path="/dashboard" element={<DashBoard/>}/>
-        <Routes path="*" element={<Error/>} />
-        <Routes path="/forgotpassword" element={<ForgotPassword/>} />
-        <Routes path="/login" element={<Login/>} />
-        <Routes path="/resetpassword" element={<ResetPassword/>} />
-        <Routes path="/searchcourse" element={<SearchCourse/>} />
-        <Routes path="/signup" element={<SignUp/>} />
-        <Routes path="/verifyotp" element={<VerifyOtp/>} />
-        <Routes path=" /viewcourse" element={<ViewCourse/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/catalog" element={<Catalog/>} />
+        <Route path="/contactus" element={<ContactUs/>} />
+        <Route path="/dashboard" element={<DashBoard/>}/>
+        <Route path="*" element={<Error/>} />
+        <Route path="/forgotpassword" element={<ForgotPassword/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/resetpassword" element={<ResetPassword/>} />
+        <Route path="/searchcourse" element={<SearchCourse/>} />
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/verifyotp" element={<VerifyOtp/>} />
+        <Route path=" /viewcourse" element={<ViewCourse/>} />
 
         
         
@@ -39,7 +39,7 @@ const App = () => {
         </Routes>
         
         </div>
-  )
+  );
 };
 
 export default App;
